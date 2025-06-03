4ee93f469815b8bdd6d3521975e580852e0f8a00
hendr1an <hendriansyahgyan@gmail.com>
2025-06-03 10:31:45 +0700

    Tambahkan kelas budi dengan pewarisan dari pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -31,3 +31,16 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public: 
+    
+    budi(int pUmur):
+    pekerja(pUmur),
+    pelajar(pUmur),
+    orang(pUmur)
+{
+    cout << "budi di buat\n" << endl;
+}
+
+};
