75fa2a416d860cde87fcb8ccd36fd9d31120d246
hendr1an <hendriansyahgyan@gmail.com>
2025-06-03 11:10:20 +0700

    Buat struktur pewarisan ganda dari pekerja dan pelajar ke budi

---
+++ SolveMultiwaris.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pumur) :
+        umur(pumur)
+    {
+        cout << "orang di buat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja :  public orang {
+public:
+
+    pekerja(int pumur) :
+       orang(pumur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar :  public orang {
+public:
+  
+    pelajar(int pumur) :
+       orang(pumur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pumur):
+      pekerja(pumur),
+      pelajar(pumur)
+     
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
