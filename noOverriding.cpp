3206c4152e60a98c032d47decdbef08cf6ba4385
hendr1an <hendriansyahgyan@gmail.com>
2025-06-03 10:56:38 +0700

    Definisikan method virtual perkenalan di baseclass

---
+++ noOverriding.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class baseclass {
+    public:
+       virtual void perkenalan() {
+          cout << "hallo saya funtion dari base class";
+
+       }
+};
+
