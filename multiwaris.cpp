90a13d685b447b4d81c4072c4988485de2dfbc57
hendr1an <hendriansyahgyan@gmail.com>
2025-06-03 10:21:55 +0700

    menambahkan class pekerja dan pelajar untuk konsep multiwaris

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,3 +11,23 @@ public:
         cout << "orang di buat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : virtual public orang {
+public:
+
+    pekerja(int pumur) :
+       orang(pumur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+  
+    pelajar(int pumur) :
+       orang(pumur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
