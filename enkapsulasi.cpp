3804fca1edfcd91d98662ace6e5e773fb4944af2
hendr1an <hendriansyahgyan@gmail.com>
2025-06-03 10:50:31 +0700

    Uji method set dan get saklarNo pada class remotelampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,20 @@ public:
         return saklarNo[1];
     }
 };
+
+int main() {
+    remotelampu lampurumah;
+
+    lampurumah.setSaklarNo(0, "lampu teras rumah");
+    lampurumah.setSaklarNo(1, "lampu ruang tamu");
+    lampurumah.setSaklarNo(2, "lampu kamar tidur");
+    lampurumah.setSaklarNo(3, "lampu dapur");
+
+    cout <<lampurumah.getSaklarNo(0) << endl;
+    cout <<lampurumah.getSaklarNo(1) << endl;
+    cout <<lampurumah.getSaklarNo(2) << endl;
+    cout <<lampurumah.getSaklarNo(3) << endl;
+
+    return 0;
+
+}
